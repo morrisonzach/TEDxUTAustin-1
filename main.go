9a7750dcc008080c98aa51998de4bdd279582524
zach morrison <morrisonizach@gmail.com>
2020-09-27 22:57:22 -0500

    switching workspaces

--- main.go
+++ main.go
@@ -4,44 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/smtp"
 )
 
-func main() {
-	http.HandleFunc("/", handle)
-	if err := http.ListenAndServe(":5500", nil); err != nil {
-		log.Fatal(err)
-	}
+//Vote is ...
+type Vote struct {
+	Name   string
+	Email  string
+	Answer string
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
+func vote(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintf(w, "invalid_http_method")
-		return
+		fmt.Println("invalid request")
 	}
-
-	r.ParseForm()
-
-	fmt.Fprintf(w, "hello, this is the form: "+r.Form.Get("name"))
-	from := "ziemboy9@gmail.com"
-	pass := "Z@chary14"
-
-	to := []string{
-		"hypemephotography@gmail.com",
+	vote := Vote{
+		Name:   r.FormValue("name"),
+		Email:  r.FormValue("email"),
+		Answer: r.FormValue("voting"),
 	}
+	fmt.Fprintf(w, "<h1>thanks for the vote "+vote.Name+"</h1><p>"+vote.Email+" "+vote.Answer+"</p>")
+}
 
-	smtpHost := "smtp.gmail.com"
-	smtpPort := "587"
-
-	message := []byte(r.Form.Get("voting"))
-	auth := smtp.PlainAuth("", from, pass, smtpHost)
-
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("email sent successfully!")
-
+func main() {
+	http.HandleFunc("/vote", vote)
+	// http.HandleFunc("/results", results)
+	fmt.Println("printing to show server is on")
+	log.Fatal(http.ListenAndServe("127.0.0.1:5500", nil))
 }
